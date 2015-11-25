beaa11e9417934e538583dba93373cf1209f148a
Woody Rousseau <woody.a.rousseau@gmail.com>
2015-11-26 00:10:19 +0100

    Updates demo to show features

--- demo.js
+++ demo.js
@@ -1,12 +1,20 @@
-angular.module('angularSmartHttpDemo', ['ngSmartHttp'])
+angular.module('angularSmartHttpDemo', ['ngSmartHttp', 'ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('first', {
+      url: "/",
+      template: '<p>{{data | json}}</p><button ui-sref="second">Second</button>',
+      controller: 'DemoCtrl'
+    })
+    .state('second', {
+      url: "/second",
+      template: '<p>{{data | json}}</p><button ui-sref="first">First</button>',
+      controller: 'DemoCtrl'
+    });
+  })
   .controller('DemoCtrl', function($scope, SmartHttp) {
     SmartHttp.get('http://bnp-ip-onecms-api-integration.bearstech.com/translation/locale/fr_FR')
     .then(function(data) {
       $scope.data = data;
-      console.log(data);
-      return SmartHttp.get('http://bnp-ip-onecms-api-integration.bearstech.com/translation/locale/fr_FR')
-    })
-    .then(function(data) {
-      console.log(data);
     });
   });
